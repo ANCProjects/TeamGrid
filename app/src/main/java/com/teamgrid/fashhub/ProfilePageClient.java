51f30f362f51b2536ac0635b26bb7b0354b17af2
Boscotec <boscotec.inc@gmail.com>
2017-08-24 17:52:21 +0100

    Edited the xml files and remove redundant classes and added authentication

--- app/src/main/java/com/teamgrid/fashhub/ProfilePageClient.java
+++ app/src/main/java/com/teamgrid/fashhub/ProfilePageClient.java
@@ -2,17 +2,36 @@ package com.teamgrid.fashhub;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.teamgrid.fashhub.utils.Device;
 
 public class ProfilePageClient extends AppCompatActivity {
+
+    String user;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference mUserDbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page_client);
 
-        EditText userNameClient = (EditText) findViewById(R.id.input_username_client);
-        userNameClient.getText().toString();
+        if(getIntent()!=null){
+            user = getIntent().getExtras().getString("user");
+        }
 
+        Device.dismissProgressDialog(this);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+      //  TextView hi = (TextView) findViewById(R.id.text);
+      //  hi.setText("Welcome ".concat(user));
     }
+
+    private void signOut() {
+        mFirebaseAuth.signOut();
+    }
+
 }
