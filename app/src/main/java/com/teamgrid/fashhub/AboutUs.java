5f0a4a47d4107fe11c9ead89407a08f6fe4f0f8b
rx-precious <opebash2012@gmail.com>
2017-09-25 22:49:25 +0100

    About Page added

--- app/src/main/java/com/teamgrid/fashhub/AboutUs.java
+++ app/src/main/java/com/teamgrid/fashhub/AboutUs.java
@@ -2,12 +2,42 @@ package com.teamgrid.fashhub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutUs extends AppCompatActivity {
+
+    private String aboutUs = getString(R.string.about_us);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+        Element versionElement = new Element();
+        versionElement.setTitle("Version 1.0");
+
+        Element adsElement = new Element();
+        adsElement.setTitle("Advertise with us");
+
+        View aboutPage = new AboutPage(this)
+                .setDescription("I love me")
+                .isRTL(false)
+                .setImage(R.drawable.fashhub_home)
+                .addItem(versionElement)
+                .addItem(adsElement)
+                .addGroup("Connect with us")
+                .addEmail("elmehdi.sakout@gmai.com")
+                .addFacebook("the.medy")
+                .addTwitter("androidngr")
+              //.addYoutube("UCdPQtdWIsg7_pi4mrRu46vA")
+                // .addPlayStore("com.ideashower.readitlater.pro")
+                //.addInstagram("medyo80")
+                .addGitHub("ANCprojects")
+                .create();
+
+        setContentView(aboutPage);
+
     }
 }
--- app/src/main/java/com/teamgrid/fashhub/MainActivity.java
+++ app/src/main/java/com/teamgrid/fashhub/MainActivity.java
@@ -3,8 +3,6 @@ package com.teamgrid.fashhub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
